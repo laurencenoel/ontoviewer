75f6393ff9da5d960fde084cd72820551cf54c33
noel <laurence.r.noel@gmail.com>
2020-04-22 15:08:12 +0300

    json

--- ontoviewer/views/default.py
+++ ontoviewer/views/default.py
@@ -3,13 +3,13 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
 def my_view(request):
-     var data =  [
-      {from: "Canada",  to: "France",  weight: 1},
-      {from: "Canada",  to: "Germany", weight: 1},
-      {from: "Canada",  to: "Italy",   weight: 1},
-      {from: "Canada",  to: "Spain",   weight:  1},
-      {from: "USA",     to: "France",  weight:  1},
-      {from: "USA",     to: "Germany", weight: 1},
-      {from: "USA",     to: "Spain",   weight: 1}
-    ]
-    return {'project': 'ontoviewer', 'jsond':data}
+    var data =  """[
+        {from: "Canada",  to: "France",  weight: 1},
+        {from: "Canada",  to: "Germany", weight: 1},
+        {from: "Canada",  to: "Italy",   weight: 1},
+        {from: "Canada",  to: "Spain",   weight:  1},
+        {from: "USA",     to: "France",  weight:  1},
+        {from: "USA",     to: "Germany", weight: 1},
+        {from: "USA",     to: "Spain",   weight: 1}
+    ]"""
+    return {'project': 'ontoviewer', 'jsond':json.load(data)}
