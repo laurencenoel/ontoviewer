1cf775d276c2778e94ad3f7121c0872899942d0d
noel <laurence.r.noel@gmail.com>
2020-04-22 16:41:06 +0300

    query

--- ontoviewer/views/default.py
+++ ontoviewer/views/default.py
@@ -10,6 +10,12 @@ def my_view(request):
         PREFIX owl: <http://www.w3.org/2002/07/owl#>        
         SELECT ?from ?to
         WHERE {
+        ?period (rdfs:subClassOf / <http://www.w3.org/2002/07/owl#someValuesFrom>)* <http://purl.obolibrary.org/obo/HsapDv_0000045> .
+        ?period <http://purl.obolibrary.org/obo/hsapdv#start_dpf> ?day2 . FILTER (?day2 < 10 ) 
+        ?period oboInOwl:hasDbXref ?startCS .
+        FILTER regex(str(?startCS), "EHDAA")
+        BIND (IRI(CONCAT("http://purl.obolibrary.org/obo/ehdaa2#",strafter(?startCS,":"))) AS ?startCS2)
+        ?child <http://purl.obolibrary.org/obo/BFO_0000068> ?startCS2 .
         ?child rdfs:subClassOf ?n . ?n owl:onProperty <http://purl.obolibrary.org/obo/RO_0002202> ; owl:someValuesFrom ?parent . 
         ?parent rdfs:label ?from .
         ?child rdfs:label ?to .
