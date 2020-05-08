9c2b967cbdb6b97e3ac8f8b3c6acdaa856db6a56
noel <laurence.r.noel@gmail.com>
2020-05-08 15:25:03 +0300

    ChildrenOxAxiom

---
+++ export/getTissueFromLocalRepo.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+
+from export.repoConf import *
+
+import sys
+import getopt
+import requests
+import csv
+
+requestURL = HCAOQUERY
+
+dicoChildParent = {}
+listAllTissue = []
+listAllOrgPart = []
+
+def print_usage():
+    """Print a help message."""
+    print(
+        """getListsFromLocalRepo.py [-h][-p] query
+This script sends SPARQL query to OBO endpoint http://openstack-192-168-100-40.genouest.org/rdf4j-server/repositories/hcao/
+and transforms the result to an openSpecimen 
+Arguments:
+    -h           Print out this help message.
+    -p           Path to the directory for results
+"""
+    )
+    
+def getAxiomChildren(broader,withLabel=False) : 
+    print("get axiom subclasses")
+    childrenList = []
+
+    query = """
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX obo-term: <http://purl.obolibrary.org/obo/>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    SELECT distinct ?s ?label {{
+    ?s rdfs:subClassOf ?s_axiom .
+    ?s_axiom owl:onProperty <http://purl.obolibrary.org/obo/BFO_0000050>  .
+    ?s_axiom owl:someValuesFrom obo-term:{broader} .  
+    ?s rdfs:label ?label .
+    FILTER NOT EXISTS {{ ?s <http://www.geneontology.org/formats/oboInOwl#hasOBONamespace> "cell" }}
+    FILTER NOT EXISTS {{?s rdfs:label ?label . FILTER(regex(?label,"cell|blast|cyte|mammalian|adult|right|left|cistern|space","i"))}}
+    }}
+    """.format(broader=broader)
+
+    #FILTER NOT EXISTS {{?s rdfs:label ?label . FILTER(regex(?label,"cell|blast|cyte|compound organ|system element|region element|segment organ|-derived structure|subdivision of|mammalian|adult|right|left","i"))}}
+ 
+    myparam = { 'query': query}
+    headers = {'Accept' : 'application/sparql-results+json'}
+    r=requests.get(requestURL,params=myparam, headers=headers)
+    results=r.json()
+
+    for row in results["results"]["bindings"] : 
+        uri = row["s"]["value"]
+        label = row["label"]["value"]
+        identifier = uri.split("/")[-1]
+        if identifier not in unique.keys() : 
+            unique[identifier] = label
+            if withLabel :                 
+                childrenList.append([identifier,label])
+            else : 
+                childrenList.append(identifier)     
+
+            childList = getChildren(identifier,withLabel)
+            if len(childList) > 1 : 
+                childrenList.extend(childList)
+       
+    return childrenList
+
+
+def getChildrenOrAxiom(broader,withLabel=False) : 
+    print("get subclasses")
+    childrenList = []
+    query = """
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX obo-term: <http://purl.obolibrary.org/obo/>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    SELECT distinct ?s ?label {{
+    {{
+    ?s rdfs:subClassOf  obo-term:{broader} . }}
+    UNION {{
+    ?s rdfs:subClassOf ?s_axiom .
+    ?s_axiom owl:onProperty <http://purl.obolibrary.org/obo/BFO_0000050>  .
+    ?s_axiom owl:someValuesFrom obo-term:{broader} . }}
+    ?s rdfs:label ?label .
+    FILTER NOT EXISTS {{ ?s <http://www.geneontology.org/formats/oboInOwl#hasOBONamespace> "cell" }}
+    FILTER NOT EXISTS {{?s rdfs:label ?label . FILTER(regex(?label,"cell|blast|cyte|mammalian|adult|right|left|cistern|space","i"))}}
+    }}
+    """.format(broader=broader)
+    
+    #FILTER NOT EXISTS {{?s rdfs:label ?label . FILTER(regex(?label,"cell|blast|cyte|compound organ|system element|region element|segment organ|-derived structure|subdivision of|mammalian|adult|right|left","i"))}}
+ 
+    myparam = { 'query': query}
+    headers = {'Accept' : 'application/sparql-results+json'}
+    r=requests.get(requestURL,params=myparam, headers=headers)
+    print(r.status_code)
+    results=r.json()
+    
+    for row in results["results"]["bindings"] : 
+        uri = row["s"]["value"]
+        label = row["label"]["value"]
+        identifier = uri.split("/")[-1]
+        if identifier not in unique.keys() : 
+            unique[identifier] = label
+            if withLabel : 
+                childrenList.append([identifier,label])
+            else : 
+                childrenList.append(identifier)       
+        
+            childL = getChildrenOrAxiom(identifier,withLabel)
+            if len(childL) > 1 : 
+                childrenList.extend(childL)
+        
+    return childrenList
+
+def addToDico(organ,prim) :
+    listElt = dico[organ]
+    listElt.append(prim)
+    dico[organ] = listElt
+
+def askParent(identifier) : 
+    if identifier in dicoChildParent.keys() : 
+        return dicoChildParent[identifier]
+    else :
+        return ["HUDECA_0000002"]
+        
+def askOrgParent(identifier) : 
+    if identifier in orgParent.keys() : 
+        return orgParent[identifier]
+    return ""      
+      
+     
+            
+def checkExceptLabel(label) : 
+    exceptLabels =  ["compound organ","system element","region element","segment organ","-derived structure","subdivision of","mammalian","adult","right","left","zone of","regional part of "]
+    for elt in exceptLabels : 
+        if elt in label : 
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    try:
+        optlist, args = getopt.getopt(sys.argv[1:], "hr")
+    except getopt.error as x:
+        print(x)
+        sys.exit(0)
+
+
+    show_help = False
+    path = ""
+    for opt, arg in optlist:
+        if opt == "-h":
+            show_help = True
+        elif opt == "-r":
+            resultPath = arg
+        elif opt == "-d":
+            sys.stderr.write("The delay parameter is now ignored\n")
+    if show_help:
+        print_usage()
+        sys.exit(0)
+
+    
+    print("Get Tissue")
+    unique = {}
+    #listAllT = getChildren("UBERON_0000479",True)
+    #listAllAxiomTop = getAxiomChildren("UBERON_0000479",True)
+    #listAllTissue = listAllT + listAllAxiomTop
+    listAllTissue = getChildrenOrAxiom("UBERON_0000479",True)
+    #MEMO Check Tissue FROM EHDAA
+    
+    #with open("PV/tissueAll.csv", "w") as fT:
+        #for tissue in listAllTissue :
+            #identifier = tissue[0]
+            #label = tissue[1]
+            #fT.write(identifier+";"+label+"\n") 
+            
+            
+    print("Get organ child")
+    orgParent = []
+    with open("PV/organ_child.csv", "r") as fo:
+        csv_reader = csv.reader(fo, delimiter=';')
+        print("skipping headers")
+        next(csv_reader)
+        for lines in csv_reader:
+            orgParent[lines[0]] = lines[1]
+
+    
+    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    print("CHECK IF IDENTIFIER IS IN ORGAN CHILD TO ADD PARENT AND CREATE FILE")
+    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    
+    
+    with open("PV/tissue_PV.csv", "w") as f:
+        f.write('IDENTIFIER,CONCEPT_CODE,SHORT_URI,DEFINITION,DESCRIPTORS,PARENTS,PARENT_IDENTIFIER,value,PUBLIC_ID\n')
+  
+    
+        for tissue in listAllTissue :        
+            identifier = tissue[0]
+            label = tissue[1]
+            descriptors = ""
+            parentStr = askOrgParent[identifier]
+            f.write('"getNextPvId(),"","'+identifier+'","","'+descriptors+'","'+parentStr+'","","'+label+'","organ"\n')
+    
