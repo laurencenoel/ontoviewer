b5a9fb6b2a2f04c6245f07ff5a35f6db3e5f5b14
noel <laurence.r.noel@gmail.com>
2020-05-13 23:15:08 +0300

    test

--- ontoviewer/views/default.py
+++ ontoviewer/views/default.py
@@ -5,17 +5,12 @@ from pyramid.events import NewResponse, subscriber
 
 @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
 def my_view(request):
-
-
-
     return {'project': 'ontoviewer'}
 
-
-
-@subscriber(NewResponse)
-def add_cors_headers(event):
-    if event.request.is_xhr:
-        event.response.headers.update({
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST,GET',
-        })
+@view_config(request_method='OPTIONS', permission='options')
+def options(self):
+    return Response(b'', headerlist=[
+        ('Access-Control-Allow-Origin', 'http://localhost:8080'),
+        ('Access-Control-Allow-Methods', 'POST'),
+        ('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization'),
+    ])
