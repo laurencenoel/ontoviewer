4c8336bfa57cd61633fac15b010077bf75fa535c
noel <laurence.r.noel@gmail.com>
2020-05-14 00:03:00 +0300

    test

--- ontoviewer/__init__.py
+++ ontoviewer/__init__.py
@@ -6,10 +6,6 @@ def main(global_config, **settings):
     """
     with Configurator(settings=settings) as config:
         config.include('pyramid_jinja2')
-        config.include('.cors')
-    
-     # make sure to add this before other routes to intercept OPTIONS
-        config.add_cors_preflight_handler()
         config.include('.routes')
         config.scan()
     return config.make_wsgi_app()
