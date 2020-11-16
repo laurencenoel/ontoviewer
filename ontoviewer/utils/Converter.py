7c88e88fa7213f204e7f1ccc9138242fe16fbe8b
noel <laurence.r.noel@gmail.com>
2020-11-16 11:59:33 +0100

    convert with link

--- ontoviewer/utils/Converter.py
+++ ontoviewer/utils/Converter.py
@@ -8,13 +8,15 @@ def organ_json():
     with open(csvfile,"r") as csvF :
         csv_reader = csv.reader(csvF, delimiter=';')
         line_count = 0
+        ontoLink = "https://www.ebi.ac.uk/ols/ontologies/cl/terms?iri=http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2F"
        
         for row in csv_reader:
             dict={}
-            dict["organ"]=row[0];
-            dict["cell_name"]=row[1];
-            dict["onto_ID"]=row[2];
-            data.append(dict);
+            dict["organ"]=row[0]
+            dict["cell_name"]=row[1]
+            id = row[2].replace("_",":")
+            dict["onto_ID"] = "<a href='"+ontoLink+row[2]+"' target='_blank'>"+id+"</a>"
+            data.append(dict)
     
     # Serializing json  
     json_object = json.dumps(data, indent = 4) 
