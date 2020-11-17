5405c64b870000f1f7a1cd83f98a5e452368f551
noel <laurence.r.noel@gmail.com>
2020-11-17 18:10:15 +0100

    int(float

--- ontoviewer/views/default.py
+++ ontoviewer/views/default.py
@@ -70,11 +70,9 @@ PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
      
     dicoCS  = {}
     for i,elt in enumerate(data) : 
-    
-        if elt["startDay"] == 0.0 : 
-            nbDay = 0
-        else : 
-            nbDay = int(elt["startDay"])
+        nbDay = int(float(elt["startDay"]))
+        if nbDay == 0 : 
+            nbDay = 1
         dicoInfo = {}
         dicoInfo["label"] = elt["label"]
         dicoInfo["comment"] = elt["comment"]
